9841b84c49eb118352e91f21b3bc053bf4fc04b2
Francisco Murillo <fmurillodev@gmail.com>
2021-11-17 12:07:09 -0300

    remove hooks loading

--- src/hooks/loadingHook.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-const LoadingHooks = () => {
-  const [loading, setLoading] = useState(true);
-  return { loading, setLoading };
-}
-
-export default LoadingHooks;
